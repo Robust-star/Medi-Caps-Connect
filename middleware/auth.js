4b3f8b547845dc9c4ed1e3704fec370a3c353dee
Raj Gupta <68466264+Robust-star@users.noreply.github.com>
2021-07-07 23:50:28 +0530

    Updated Token

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header('x-auth-token');
 
-  // Check if not token
+  // Check if token is not valid 
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
